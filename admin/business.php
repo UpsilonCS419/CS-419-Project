6a21c3da2428bfe32408936a314ebbef1097b198
Suyana Lozada <slozadag@gmail.com>
2016-05-23 19:37:15 -0700

    business intelligence

--- admin/business.php
+++ admin/business.php
@@ -1,12 +1,62 @@
+
+<?PHP
+include("mysqlconnect.php");
+db_connect();
+error_reporting(E_ALL);
+?>
+
 <!DOCTYPE html>
 <html>
 
 <head>
-  <title>Upsilon Employee Recognition</title>
-  <meta charset="UTF-8">
-  <link href="site.css" rel="stylesheet">
-</head>
+ <title>Upsilon Employee Recognition</title>
+ <meta charset="UTF-8">
+ <link href="site.css" rel="stylesheet">
+    <!--Load the AJAX API-->
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      // Load the Visualization API and the corechart package.
+      google.charts.load('current', {'packages':['corechart']});
 
+      // Set a callback to run when the Google Visualization API is loaded.
+      google.charts.setOnLoadCallback(drawChart);
+
+      // Callback that creates and populates a data table,
+      // instantiates the pie chart, passes in the data and
+      // draws it.
+      function drawChart() {
+
+		 var data = new google.visualization.arrayToDataTable([
+		  ['Award Type', 'Number of awards'],
+
+		  <?php
+				
+			$result = mysql_query("SELECT (SELECT title from award_type where id=tid)AS
+									awardType,count(tid)AS count FROM award GROUP BY tid");
+			while($row=mysql_fetch_assoc($result))
+			{
+				echo "['".$row['awardType']."',".$row['count']."],";	
+			}	
+			?>	
+			
+
+		]);
+   		var options = {
+       		title: 'Number of awards per type',
+       		chartArea: {width: '50%'},
+       		hAxis: {
+         	title: 'Number of awards',
+        	minValue: 0
+        	},
+      	};
+
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
+    </script>
+</head>
 <body>
 
 <h1>Upsilon Employee Recognition</h1>
@@ -17,45 +67,7 @@
   <a href="url" class="logoutLblPos">sign out</a>
 
 
-<h2>Business Intelligence Under Construction!</h2>
-
-
-<?PHP
-/*
-include("mysqlconnect.php");
-db_connect();
-
-error_reporting(E_ALL);
-$result = mysql_query("SELECT * FROM admin");
-
-echo "<h2>Administrators </h2>";
-echo "<table>";
-echo "<tr>";
-  echo "<th>Admin Account</th>";
-
-echo "</tr>";
-while($row = mysql_fetch_array($result))
-{
-	echo "<tr>";
-   	echo "<td>";
-	echo $row['email'];
-    echo "</td>";
-	echo "<td>";
-   	echo "<a href=\"edit_admin_form.php?id=".$row['id']."\">Edit</a>"; 
-	echo "</td>";
-   	echo "<td>";
-   	echo "<a href=\"delete_admins.php?id=".$row['id']."\">Delete</a>"; 
-   	echo "</td>";
-  	echo "</tr>";
-}
-echo "</table>";
-
-*/
-
-
-?>
-
-
+<div id="chart_div"/>
 <script src="script.js"></script>
 
 </body>
