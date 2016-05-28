5c4dbe272778032431b78470a19d7e9c2098cf05
Suyana Lozada <slozadag@gmail.com>
2016-05-27 17:42:04 -0700

    fix delete bug for users that created awards

--- 419-online/delete_users.php
+++ 419-online/delete_users.php
@@ -20,20 +20,52 @@ error_reporting(E_ALL);
 
 $id = $_GET['id'];
 
-$sql = "DELETE FROM user where id='".$id."'";
-
-$result = mysql_query($sql);
-if (!$result) 
+$query1="SELECT id FROM award where uid='".$id."'";
+$awards=mysql_query($query1);
+if ($awards)
 {
-    echo "Sorry this record cannot be deleted from the database";
-	echo "";
-	echo "<a href=\"users.php\">Back to Manage Customers</a>";
-
+	//If there are awards associated with a user  
+	while($row = mysql_fetch_array($awards))
+	{
+		$sql = "DELETE FROM award where id='".$row['id']."'";
+		$result= mysql_query($sql);
+		if (!$result)
+		{
+    		echo "Sorry this record cannot be deleted from the database";
+			echo "";
+			echo "<a href=\"users.php\">Back to Manage Customers</a>";
+		}
+	}	
+		$sql1 = "DELETE FROM user where id='".$id."'";
+		$result1 = mysql_query($sql1);
+		if (!$result1)
+		{
+    		echo "Sorry this record cannot be deleted from the database";
+			echo "";
+			echo "<a href=\"users.php\">Back to Manage Customers</a>";
+		}
+		else
+		{
+			echo "record has been deleted successfully, please wait while redirecting...";
+			echo "<script type='text/javascript'> document.location = 'users.php'; </script>";
+		}
 }
 else
 {
-	echo "Record has been deleted successfully, please wait while redirecting...";
-	echo "<script type='text/javascript'> document.location = 'users.php'; </script>";
+	$sql = "DELETE FROM user where id='".$id."'";
+
+	$result = mysql_query($sql);
+	if (!$result) 
+	{
+    	echo "Sorry this record cannot be deleted from the database";
+		echo "";
+		echo "<a href=\"users.php\">Back to Manage Customers</a>";
+	}
+	else
+	{
+		echo "record has been deleted successfully, please wait while redirecting...";
+		echo "<script type='text/javascript'> document.location = 'users.php'; </script>";
+	}
 }
 ?>
 
